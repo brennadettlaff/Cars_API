5bb63ca9adf8af4d1cf457a0b8e2c3ce9ed80c19
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-23 14:56:12 -0600

    updated status codes + condensed post code

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
 
@@ -9,12 +10,9 @@ def cars_list(request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
-
-
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
